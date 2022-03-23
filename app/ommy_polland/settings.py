fb02dee2bd95a6a13e5f9cfcf2881ef327f9eb8d
Nikita Zaretski <xal9wIII4ik@yandex.ru>
2022-03-24 00:09:02 +0300

    add cors origins

--- app/ommy_polland/settings.py
+++ app/ommy_polland/settings.py
@@ -17,8 +17,6 @@ DEBUG = os.environ.get('DEBUG', default=True)  # type: ignore
 
 ALLOWED_HOSTS: tp.List[str] = os.environ.get('ALLOWED_HOSTS', default='').split(',')  # type: ignore
 
-CORS_ALLOWED_ORIGINS = ['*']
-
 # Application definition
 
 INSTALLED_APPS = [
