1d8533231d10b0335ec8f40c8f6a0db6ac0f94a0
Nikita Zaretski <xal9wIII4ik@yandex.ru>
2022-03-27 16:23:26 +0300

    update urls

--- app/ommy_polland/urls.py
+++ app/ommy_polland/urls.py
@@ -3,16 +3,11 @@ from django.urls import path, include
 from django.conf.urls.static import static
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
-from api.authenticate.views import CookieTokenObtainPairView, CookieTokenRefreshView
-
 from ommy_polland.yasg import urlpatterns as doc_urls
 from ommy_polland import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-
-    path('token/', CookieTokenObtainPairView.as_view(), name='token'),
-    path('token_refresh/', CookieTokenRefreshView.as_view(), name='token_refresh'),
 
     path('api/', include(('api.account.urls', 'account'), namespace='account')),
     path('api/', include(('api.order.urls', 'order'), namespace='order')),
