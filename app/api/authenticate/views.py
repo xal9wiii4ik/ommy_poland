39e30ab1094caaef7c7340861532f128eb64935f
Nikita Zaretski <xal9wIII4ik@yandex.ru>
2022-03-30 20:17:50 +0300

    remove prints

--- app/api/authenticate/views.py
+++ app/api/authenticate/views.py
@@ -28,7 +28,7 @@ class CookieTokenObtainPairView(TokenObtainPairView):
     def finalize_response(self, request, response, *args, **kwargs):
         if response.data.get('refresh'):
             cookie_max_age = 3600 * settings.REFRESH_TOKEN_LIFETIME
-            response.set_cookie('refresh', response.data['refresh'], max_age=cookie_max_age, samesite='Strict')
+            response.set_cookie('refresh', response.data['refresh'], max_age=cookie_max_age, httponly=True)
             print(response.cookies)
             del response.data['refresh']
         return super().finalize_response(request, response, *args, **kwargs)
@@ -42,10 +42,9 @@ class CookieTokenRefreshView(TokenRefreshView):
     serializer_class = CookieTokenRefreshSerializer
 
     def finalize_response(self, request, response, *args, **kwargs):
-        print(request.COOKIES)
         if response.data.get('refresh'):
             cookie_max_age = 3600 * settings.REFRESH_TOKEN_LIFETIME
-            response.set_cookie('refresh', response.data['refresh'], max_age=cookie_max_age, samesite='Strict')
+            response.set_cookie('refresh', response.data['refresh'], max_age=cookie_max_age, httponly=True)
             del response.data['refresh']
         return super().finalize_response(request, response, *args, **kwargs)
 
