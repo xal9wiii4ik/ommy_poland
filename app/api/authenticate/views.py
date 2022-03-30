e9d6260dc6041f8c0e6740b61dc4ce525452c092
Nikita Zaretski <xal9wIII4ik@yandex.ru>
2022-03-30 22:10:15 +0300

    test

--- app/api/authenticate/views.py
+++ app/api/authenticate/views.py
@@ -45,7 +45,7 @@ class CookieTokenRefreshView(TokenRefreshView):
         if response.data.get('refresh'):
             cookie_max_age = 3600 * settings.REFRESH_TOKEN_LIFETIME
             response.set_cookie('refresh', response.data['refresh'], max_age=cookie_max_age, httponly=True,
-                                samesite=None)
+                                domain='localhost')
             del response.data['refresh']
         return super().finalize_response(request, response, *args, **kwargs)
 
