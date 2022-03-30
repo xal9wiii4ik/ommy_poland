dc0d7e5033db64cd3e99d026c1eb178860f92a72
Nikita Zaretski <xal9wIII4ik@yandex.ru>
2022-03-30 22:14:00 +0300

    test

--- app/api/authenticate/views.py
+++ app/api/authenticate/views.py
@@ -42,8 +42,8 @@ class CookieTokenRefreshView(TokenRefreshView):
 
     serializer_class = CookieTokenRefreshSerializer
 
-    def finalize_response(self, request, response, *args, **kwargs):
-        print(request.cookies)
+    def finalize_response(self, request: Request, response, *args, **kwargs):
+        print(request.COOKIES)
         if response.data.get('refresh'):
             cookie_max_age = 3600 * settings.REFRESH_TOKEN_LIFETIME
             response.set_cookie('refresh', response.data['refresh'], max_age=cookie_max_age, httponly=True,
