f628bcf2f826db4e41e64cc7e6e6577eb74c2ed5
Nikita Zaretski <xal9wIII4ik@yandex.ru>
2022-03-30 20:07:26 +0300

    check

--- app/api/authenticate/views.py
+++ app/api/authenticate/views.py
@@ -28,7 +28,7 @@ class CookieTokenObtainPairView(TokenObtainPairView):
     def finalize_response(self, request, response, *args, **kwargs):
         if response.data.get('refresh'):
             cookie_max_age = 3600 * settings.REFRESH_TOKEN_LIFETIME
-            response.set_cookie('refresh', response.data['refresh'], max_age=cookie_max_age)
+            response.set_cookie('refresh', response.data['refresh'], max_age=cookie_max_age, samesite='Lax')
             print(response.cookies)
             del response.data['refresh']
         return super().finalize_response(request, response, *args, **kwargs)
@@ -45,7 +45,7 @@ class CookieTokenRefreshView(TokenRefreshView):
         print(request.COOKIES)
         if response.data.get('refresh'):
             cookie_max_age = 3600 * settings.REFRESH_TOKEN_LIFETIME
-            response.set_cookie('refresh', response.data['refresh'], max_age=cookie_max_age)
+            response.set_cookie('refresh', response.data['refresh'], max_age=cookie_max_age, samesite='Lax')
             del response.data['refresh']
         return super().finalize_response(request, response, *args, **kwargs)
 
