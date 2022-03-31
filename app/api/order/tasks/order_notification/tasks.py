991378f38b7a4a9cceade81fae197100ef35a6e6
Nikita Zaretski <xal9wIII4ik@yandex.ru>
2022-03-31 11:53:57 +0300

    update sending master message with new order

--- app/api/order/tasks/order_notification/tasks.py
+++ app/api/order/tasks/order_notification/tasks.py
@@ -17,12 +17,23 @@ def send_notification_with_new_order_to_masters(order_pk: int, masters_phone_num
         masters_phone_numbers: masters phone numbers
     """
 
+    from api.order.models import Order
+
+    order = Order.objects.get(pk=order_pk)
     client = Client(settings.TWILIO_ACCOUNT_SID, settings.TWILIO_AUTH_TOKEN)
 
-    # TODO update send message(mb link to site)
+    # TODO update link
+    message = f'Новая заявка!\n' \
+              f'Описание: {order.name}\n' \
+              f'Когда нужно выполнить заказ: {order.start_time:%Y-%m-%d %H-%M}\n' \
+              f'Предварительная цена: {order.price}\n' \
+              f'Комиссия сервиса: a lot of money\n' \
+              f'Стоимость без комиссии: not enough money\n' \
+              f'Link to confirm order: domain/some_link/order_pk/'
     for master_phone_number in masters_phone_numbers:
+        # TODO add count; add sleep some time
         _ = client.messages.create(
             to=master_phone_number,
             from_=settings.TWILIO_PHONE_NUMBER,
-            body=f'You have new order, pk: {order_pk}'
+            body=message
         )
