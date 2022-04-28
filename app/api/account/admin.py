32e59e2c1891f26a6ddc7cdfd29d14dce2f6c20f
Nikita Zaretski <xal9wIII4ik@yandex.ru>
2022-04-28 15:29:57 +0300

    update admin panel

--- app/api/account/admin.py
+++ app/api/account/admin.py
@@ -9,5 +9,9 @@ class UserModelAdmin(admin.ModelAdmin):
     Display table User on admin panel
     """
 
-    list_display = ('id', 'first_name', 'middle_name', 'phone_number', 'is_master')
+    @staticmethod
+    def name(user: User):
+        return f'{user.first_name} {user.last_name}'
+
+    list_display = ('id', 'name', 'phone_number', 'is_master')
     exclude = ['is_superuser', 'is_staff', 'groups', 'user_permissions', 'password']
