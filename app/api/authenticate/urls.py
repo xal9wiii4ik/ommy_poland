ae85f2344fcf895c047f5f80df0203b48b4ff2f3
Nikita Zaretski <xal9wIII4ik@yandex.ru>
2022-05-18 23:31:54 +0300

    add url and view for validating access token

--- app/api/authenticate/urls.py
+++ app/api/authenticate/urls.py
@@ -6,11 +6,13 @@ from api.authenticate.views import (
     ResendingActivatingCodeApiView,
     ActivateAccountApiView,
     CheckActivationCodeApiView,
+    ValidateAccessTokenApiView,
 )
 
 urlpatterns = [
     path('token/', CookieTokenObtainPairView.as_view(), name='token'),
     path('token_refresh/', CookieTokenRefreshView.as_view(), name='token_refresh'),
+    path('validate_access_token/', ValidateAccessTokenApiView.as_view(), name='validate_access_token'),
 
     path('auth/activate/', ActivateAccountApiView.as_view(), name='activate'),
     path('auth/check_activation_code/', CheckActivationCodeApiView.as_view(), name='check_activation_code'),
