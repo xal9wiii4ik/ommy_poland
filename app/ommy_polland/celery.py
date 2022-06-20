54ae84fbfa6f17e8ff210f9ed75bcf42fc4b4b60
Nikita Zaretski <xal9wIII4ik@yandex.ru>
2022-06-20 16:06:42 +0300

    add beat task

--- app/ommy_polland/celery.py
+++ app/ommy_polland/celery.py
@@ -3,6 +3,8 @@ import os
 from celery import Celery
 from glob import glob
 
+from celery.schedules import crontab
+
 from ommy_polland import settings
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ommy_polland.settings')
@@ -11,3 +13,9 @@ TASKS_FILES = map(lambda x: x.replace('/tasks.py', '').replace('/', '.'), glob('
 celery_app = Celery('ommy_polland', broker=settings.CELERY_BROKER_URL, backend=settings.CELERY_RESULT_BACKEND)
 celery_app.config_from_object('django.conf:settings', namespace='CELERY')
 celery_app.autodiscover_tasks(TASKS_FILES, force=True)
+celery_app.conf.beat_schedule = {
+    'remove-old-codes': {
+        'task': 'authenticate.remove_old_codes',
+        'schedule': crontab(hour='0', minute='0')
+    },
+}
