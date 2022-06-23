52cd0d611e6798a223e1e5ca5cf99db0912d178f
Nikita Zaretski <xal9wIII4ik@yandex.ru>
2022-06-23 21:57:48 +0300

    add address to serializer

--- app/api/master/serializers.py
+++ app/api/master/serializers.py
@@ -17,7 +17,7 @@ class MasterRegisterSerializer(UserRegisterSerializer):
     """ Serializer for register master """
 
     master_experience = MasterExperienceModelSerializer(many=True, required=False)
-    master_address = serializers.CharField(max_length=100, required=False)
+    address = serializers.CharField(max_length=100, required=False)
     longitude = serializers.DecimalField(max_digits=9, decimal_places=6, required=True)
     latitude = serializers.DecimalField(max_digits=9, decimal_places=6, required=True)
     city = serializers.CharField(max_length=100, required=True)
